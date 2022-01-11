1538cbf8dab79ce03e301d14a42d71ca19ec1133
Jayson Byrd <jayson_byrd_95@outlook.com>
2022-01-11 16:03:28 -0500

    Initial Commit - GET posts completed

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,24 @@
+/* eslint-disable no-unused-vars */
 // implement your posts router here
+const express = require('express')
+const router = express.Router()
+
+const Post = require('./posts-model.js')
+
+
+router.get('/', (req, res) => {
+    Post.find()
+    .then(post => {
+        res.status(200).json(post)
+    })
+    .catch(err => {
+        res.status(500).json({message: "The posts information could not be retrieved"})
+    })
+})
+
+
+
+
+
+module.exports = router
+

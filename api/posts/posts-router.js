1ba16fc83d62fa602c481f7a703c472dd7be7148
Jayson Byrd <jayson_byrd_95@outlook.com>
2022-01-11 17:47:11 -0500

    DELETE removes post from database

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -41,6 +41,38 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.put('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const { title, contents } = req.body
+        const updatedPost = await Post.update(id, {title, contents})
+        if(!updatedPost) {
+            return res.status(404).json({message: "The post with the specified ID does not exist"})
+        }else if(!title || !contents) {
+            return res.status(400).json({message: "Please provide title and contents for the post"})
+        } else {
+            res.status(200).json(updatedPost)
+        }
+    } catch(err) {
+        return res.status(500).json({message:"The post information could not be modified"})
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const deletedPost = await Post.remove(id)
+        if(!deletedPost) {
+            return res.status(404).json({message: "The post could not be removed"})
+        } else {
+            res.status(200).json(deletedPost)
+        }
+
+    } catch(err) {
+        res.status(500).json({message: "The comments information could not be retrieved"})
+    }
+})
+
 
 
 
